7c0fbe3f5c74f62545c20ab0f7c2ffcae68b0e7c
WangPinChen <r17369@hotmail.com>
2023-09-18 17:02:11 +0800

    project init

---
+++ app.js
@@ -0,0 +1,19 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const express = require('express')
+const handlebars = require('express-handlebars')
+const routes = require('./routes')
+const app = express()
+const port = process.env.PORT || 3000
+const http = require("http")
+const server = http.createServer(app)
+const { Server } = require("socket.io")
+const io = new Server(server)
+
+app.set('view engine', 'hbs')
+app.engine("hbs", handlebars({ defaultLayout: 'main', extname: ".hbs" }))
+app.use("/", express.static("public"))
+app.use(routes)
+
+server.listen(port, () => console.log(`Example app listening on port ${port}!`))
